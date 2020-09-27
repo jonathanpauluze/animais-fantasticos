343745bb34996633b581aad82d482bbf361a60c6
Jonathan Pauluze <jonathanpauluze@gmail.com>
2020-09-27 00:39:18 -0300

    refactor: change smooth scroll into class

---
+++ js/modules/smooth-scroll.js
@@ -0,0 +1,37 @@
+export default class SmoothScroll {
+  constructor(links, options) {
+    this.internalLinks = document.querySelectorAll(links);
+
+    if (!options) {
+      this.options = {
+        behavior: 'smooth',
+        block: 'start',
+      };
+    } else {
+      this.options = options;
+    }
+
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.internalLinks.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
+    });
+  }
+
+  init() {
+    if (this.internalLinks.length) {
+      this.addLinkEvent();
+    }
+
+    return this;
+  }
+}
