2c05ae4b4fd6d7a4677773a929bca90c5cd4b4f2
Jonathan Pauluze <jonathanpauluze@gmail.com>
2020-10-08 23:56:21 -0300

    refactor: change function to improve performance

--- js/modules/animate-on-scroll.js
+++ js/modules/animate-on-scroll.js
@@ -3,23 +3,41 @@ export default class AnimateOnScroll {
     this.sections = document.querySelectorAll(sections);
     this.halfWindow = window.innerHeight * 0.6;
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistances = this.checkDistances.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - this.halfWindow) < 0;
-      if (isSectionVisible) {
-        section.classList.add('active');
-      } else if (section.classList.contains('active')) {
-        section.classList.remove('active');
+  getDistances() {
+    this.distances = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+
+      return {
+        element: section,
+        offset: Math.floor(offset) - this.halfWindow,
+      };
+    });
+  }
+
+  checkDistances() {
+    this.distances.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('active');
+      } else if (item.element.classList.contains('active')) {
+        item.element.classList.remove('active');
       }
     });
   }
 
+  destroy() {
+    window.removeEventListener('scroll', this.checkDistances);
+  }
+
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistances();
+      this.checkDistances();
+      window.addEventListener('scroll', this.checkDistances);
+    }
+
+    return this;
   }
 }
