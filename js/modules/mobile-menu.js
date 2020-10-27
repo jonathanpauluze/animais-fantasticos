393f4bf48a435c06b2c85c992b7af18fb6606ac4
Jonathan Pauluze <jonathanpauluze@gmail.com>
2020-10-26 21:55:13 -0300

    refactor: change mobile menu into class

---
+++ js/modules/mobile-menu.js
@@ -0,0 +1,37 @@
+import outsideClick from './outsideclick.js';
+
+export default class MobileMenu {
+  constructor({ menuButton, menuList, events = ['click', 'touchstart'] }) {
+    this.menuButton = document.querySelector(menuButton);
+    this.menuList = document.querySelector(menuList);
+    this.events = events;
+    this.activeClass = 'active';
+
+    this.openMenu = this.openMenu.bind(this);
+  }
+
+  openMenu() {
+    this.menuList.classList.add(this.activeClass);
+    this.menuButton.classList.add(this.activeClass);
+
+    outsideClick(this.menuList, this.events, () => {
+      this.menuList.classList.remove(this.activeClass);
+      this.menuButton.classList.remove(this.activeClass);
+    });
+  }
+
+  addMobileMenuEvents() {
+    this.events.forEach(event => this.menuButton.addEventListener(event, this.openMenu));
+  }
+
+  init() {
+    console.log(this.menuButton);
+    console.log(this.menuList);
+
+    if (this.menuButton && this.menuList) {
+      this.addMobileMenuEvents();
+    }
+
+    return this;
+  }
+}
